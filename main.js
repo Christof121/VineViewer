3330553e13b576284b16760e4be2a2223bc2d44f
Christof <114830546+Christof121@users.noreply.github.com>
2023-08-30 14:56:15 +0200

    Update main.js
    Version 1.0
    Amazon Vine Viewer -> Vine Viewer

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Vine Viewer
 // @namespace    http://tampermonkey.net/
-// @version      Beta-1.0
+// @version      1.0
 // @description  Erweiterung der Produkt Übersicht von Amazon Vine
 // @author       Christof
 // @match        *://www.amazon.de/vine/*
